78c7152455c4dfc4cf761fb522215f6559468258
TimurKarev <timurkarev@mail.ru>
2020-09-14 16:04:32 +0300

    Add to do to plan test

--- test.py
+++ test.py
@@ -13,8 +13,9 @@ while i < len(ColumnNames)-2:
     l.append(ColumnNames[i][:-4])
     i+=2
 
-data = ref.get_df_for_gantt(10)
+data = ref.get_df_for_gantt(20)
 
+#TODO придумать, как показывать наложившиеся друг на бруга бары
 fig = px.timeline(data, x_start="Start", x_end="Finish", y="Task", color='zakaz', category_orders={'Task':l})
 #fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
 fig.update_xaxes(
